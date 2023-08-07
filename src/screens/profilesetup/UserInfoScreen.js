97b1e08e46286c4c052d587c5e48447340fd7934
joy kirii <joyki@MacBook-Pro.local>
2023-08-07 17:29:41 +0100

    cleaned up userinfo screen

--- src/screens/profilesetup/UserInfoScreen.js
+++ src/screens/profilesetup/UserInfoScreen.js
@@ -1,24 +1,26 @@
-import React, {useState, useCallback, pi} from 'react';
-import {View, StyleSheet, Image, ImageBackground} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import * as Yup from 'yup';
 import {yupResolver} from '@hookform/resolvers/yup';
 import {useForm} from 'react-hook-form';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import DatePicker from 'react-native-date-picker';
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
-// import ImagePicker from 'react-native-image-picker';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import * as ImagePicker from 'expo-image-picker';
+
 //Local imports
 import AppInput from '../../components/AppInput';
 import AppText from '../../components/AppText';
 import colors from '../../config/colors';
 import PrimaryButton from '../../components/PrimaryButton';
-import ImagePickerModal from '../../components/ImagePickerModal';
 
-//Validation Schema for login form, validates email and password
+//Validation Schema for login form, validates first name, last name and date of birth
 const validationSchema = Yup.object().shape({
   firstName: Yup.string()
     .matches(/^[A-Za-z ]*$/, 'Please enter valid name')
@@ -31,7 +33,6 @@ const validationSchema = Yup.object().shape({
   dob: Yup.date().required().label('dob'),
 });
 
-const image = require('../../../assets/profileBackground.png');
 //Using the useForm hook for form opeations and validation
 function UserInfoScreen({navigation}) {
   const {
@@ -42,15 +43,13 @@ function UserInfoScreen({navigation}) {
     resolver: yupResolver(validationSchema),
   });
 
-  //IMAGE PICKER
-  // eslint-disable-next-line prettier/prettier
-
   const placeholderImage = require('../../../assets/profileBackground.png');
-  // eslint-disable-next-line no-bitwise, prettier/prettier, no-undef
-  // const [image, setImage] = useState(null);
+
+  //State to manage the selected image for the profile picture
   const [selectedImage, setSelectedImage] = useState(null);
+
+  //Function to launch image picker and select an image
   const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
@@ -64,101 +63,73 @@ function UserInfoScreen({navigation}) {
       setSelectedImage(result.assets[0].uri);
     }
   };
-  // //Image picker
-  // const [pickerResponse, setPickerResponse] = useState(null);
-  // const [visible, setVisible] = useState(false);
-
-  // const onImageLibraryPress = useCallback(() => {
-  //   const options = {
-  //     selectionLimit: 1,
-  //     mediaType: 'photo',
-  //     includeBase64: false,
-  //   };
-  //   ImagePicker.launchImageLibrary(options, response =>
-  //     setPickerResponse(response),
-  //   );
-  // }, []);
-
-  // const onCameraPress = useCallback(() => {
-  //   const options = {
-  //     saveToPhotos: true,
-  //     mediaType: 'photo',
-  //     includeBase64: false,
-  //   };
-  //   ImagePicker.launchCamera(options, response => setPickerResponse(response));
-  // }, []);
-
-  // const uri = pickerResponse?.assets?.[0]?.uri;
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.imageContainer}>
-        <View style={styles.profileContainer}>
-          <TouchableOpacity onPress={pickImage}>
-            {selectedImage ? (
-              <ImageBackground
-                source={{uri: selectedImage}}
-                resizeMode="cover"
-                style={styles.profilePicture}>
-                <MaterialCommunityIcons
-                  name="camera-account"
-                  size={25}
-                  style={styles.imagePicker}
-                />
-              </ImageBackground>
-            ) : (
-              <ImageBackground
-                source={placeholderImage}
-                resizeMode="cover"
-                style={styles.profilePicture}>
-                <MaterialCommunityIcons
-                  name="camera-account"
-                  size={25}
-                  style={styles.imagePicker}
-                />
-              </ImageBackground>
-            )}
-            {/*  {image && (
-              <ImageBackground resizeMode="cover" style={styles.profilePicture}>
-                <MaterialCommunityIcons
-                  name="camera-account"
-                  size={25}
-                  style={styles.imagePicker}
-                />
-              </ImageBackground>
-            )} */}
-          </TouchableOpacity>
+    <KeyboardAvoidingView
+      style={{flex: 1}}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.imageContainer}>
+          <View style={styles.profileContainer}>
+            {/* Profile Picture */}
+            <TouchableOpacity onPress={pickImage}>
+              {selectedImage ? (
+                <ImageBackground
+                  source={{uri: selectedImage}}
+                  resizeMode="cover"
+                  style={styles.profilePicture}>
+                  <MaterialCommunityIcons
+                    name="camera-account"
+                    size={25}
+                    style={styles.imagePicker}
+                  />
+                </ImageBackground>
+              ) : (
+                <ImageBackground
+                  source={placeholderImage}
+                  resizeMode="cover"
+                  style={styles.profilePicture}>
+                  <MaterialCommunityIcons
+                    name="camera-account"
+                    size={25}
+                    style={styles.imagePicker}
+                  />
+                </ImageBackground>
+              )}
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-      <View style={styles.form}>
-        {/* First Name */}
-        <AppInput
-          control={control}
-          name="firstName"
-          rules={{required: true}}
-          placeholder="First Name"
-          style={styles.input}
-        />
-        {errors.firstName && <AppText>{errors.firstName.message}</AppText>}
-        {/* Last Name */}
-        <AppInput
-          control={control}
-          name="lastName"
-          rules={{required: true}}
-          placeholder="Last Name"
-          style={styles.input}
-        />
-        {errors.lastName && <AppText>{errors.lastName.message}</AppText>}
-      </View>
-      <View style={styles.button}>
-        <PrimaryButton
-          label="Continue"
-          onPress={() => navigation.navigate('UserInterests')}
-        />
-      </View>
-    </SafeAreaView>
+        <View style={styles.form}>
+          {/* First Name */}
+          <AppInput
+            control={control}
+            name="firstName"
+            rules={{required: true}}
+            placeholder="First Name"
+            style={styles.input}
+          />
+          {errors.firstName && <AppText>{errors.firstName.message}</AppText>}
+          {/* Last Name */}
+          <AppInput
+            control={control}
+            name="lastName"
+            rules={{required: true}}
+            placeholder="Last Name"
+            style={styles.input}
+          />
+          {errors.lastName && <AppText>{errors.lastName.message}</AppText>}
+        </View>
+        <View style={styles.button}>
+          <PrimaryButton
+            label="Continue"
+            onPress={() => navigation.navigate('UserInterests')}
+          />
+        </View>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
 
+//Stylesheet
 const styles = StyleSheet.create({
   container: {
     flex: 1,
