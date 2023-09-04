911c5c8c20b4b5c1b6df8c8971c0ab866022f4e8
joy kirii <joyki@pc-203-102.customer.ask4.lan>
2023-09-04 13:32:48 +0100

    fix bugs

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  presets: ['babel-preset-expo'],
+  presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+  plugins: ['react-native-reanimated/plugin'],
 };
