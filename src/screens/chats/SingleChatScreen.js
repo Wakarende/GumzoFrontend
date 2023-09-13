65a8d11390980202112cbef17ceeae6bd43d8e24
Joy Kirii <joykirii@gmail.com>
2023-09-13 21:14:16 +0100

    fixed chat bubble bug

--- src/screens/chats/SingleChatScreen.js
+++ src/screens/chats/SingleChatScreen.js
@@ -1,125 +1,3 @@
-// import React, {useState, useEffect} from 'react';
-// import {GiftedChat} from 'react-native-gifted-chat';
-// import {View, StyleSheet} from 'react-native';
-
-// //local imports
-// import CustomChatInput from '../../components/inputs/CustomChatInput';
-// import AudioMessagePlayer from '../../components/AudioMessagePlayer';
-// import BackArrow from '../../components/BackArrow';
-// import ChatActions from '../../components/ChatActions';
-// import ChatBubble from '../../components/ChatBubble';
-// import {useAudioRecording} from '../../hooks/useAudioRecording';
-// import {useFirebaseChat} from '../../hooks/useFirebaseChat';
-
-// function SingleChatScreen({navigation, route}) {
-//   const [isUnloaded, setIsUnloaded] = useState(false);
-
-//   // const [messages, setMessages] = useState([]);
-//   // const [userId, setUserId] = useState(null);
-//   const otherUserId = route.params.otherUserId;
-//   console.log('Other User ID:', otherUserId);
-
-//   // Use the custom hook for audio recording logic
-//   const {
-//     recording,
-//     isRecording,
-//     audioURI,
-//     isAudioReadyToSend,
-//     startRecording,
-//     stopRecording,
-//     sendAudioMessage,
-//   } = useAudioRecording();
-
-//   // Use the custom hook for Firebase chat management
-//   const {messages, userId, onSend} = useFirebaseChat(otherUserId);
-
-//   useEffect(() => {
-//     return () => {
-//       //stop and unload the recording when the component unmounts if it's currently recording
-//       if (isRecording && recording && !isUnloaded) {
-//         recording.stopAndUnloadAsync().then(() => {
-//           setIsUnloaded(true);
-//         });
-//       }
-//     };
-//   }, [isRecording, recording, isUnloaded]);
-
-//   console.log('Messages: ', messages);
-//   return (
-//     <View style={styles.container}>
-//       <View style={styles.header}>
-//         <BackArrow
-//           onPress={() => navigation.navigate('Chats')}
-//           style={styles.backArrow}
-//         />
-//       </View>
-//       <GiftedChat
-//         messages={messages}
-//         renderActions={() => (
-//           <ChatActions
-//             onStartRecording={startRecording}
-//             onStopRecording={stopRecording}
-//           />
-//         )}
-//         onSend={messages => onSend(messages)}
-//         user={{
-//           _id: userId,
-//         }}
-//         renderInputToolbar={props => (
-//           <CustomChatInput
-//             {...props}
-//             onSendMessage={onSend}
-//             startRecording={startRecording}
-//             stopRecording={stopRecording}
-//             isAudioReadyToSend={isAudioReadyToSend}
-//             userId={userId}
-//             otherUserId={otherUserId}
-//           />
-//         )}
-//         renderMessageAudio={message => (
-//           <AudioMessagePlayer audioURL={message.currentMessage.audio} />
-//         )}
-//         style={styles.giftedChat}
-//         renderBubble={props => <ChatBubble {...props} />}
-//       />
-//     </View>
-//   );
-// }
-
-// //Stylesheet
-// const styles = StyleSheet.create({
-//   backArrow: {
-//     position: 'absolute',
-//     zIndex: 10,
-//     top: 10,
-//     left: 10,
-//     backgroundColor: 'rgba(255,255,255,0.6)',
-//   },
-//   container: {
-//     flex: 1,
-//     // paddingVertical: 3
-//     paddingBottom: 30,
-//     justifyContent: 'center',
-//     // alignItems: 'center',
-//   },
-//   giftedChat: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   header: {
-//     paddingHorizontal: 15,
-//     paddingVertical: 10,
-//   },
-//   iconContainer: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     columnGap: 5,
-//     width: 100,
-//   },
-// });
-// export default SingleChatScreen;
-
 import React, {useState, useCallback, useEffect} from 'react';
 import {GiftedChat, Bubble} from 'react-native-gifted-chat';
 import {View, StyleSheet, TouchableOpacity} from 'react-native';
@@ -306,14 +184,19 @@ function SingleChatScreen({navigation, route}) {
 
   //Function to stop recording
   const stopRecording = useCallback(async () => {
+    console.log('Stop recording function called');
     if (!isRecording) return;
-    console.log('stop recording');
+    console.log('is recording');
     try {
       if (recording && !isUnloaded) {
+        console.log('Recording exists and not unloaded');
         await recording.stopAndUnloadAsync();
+        console.log('Recording stopped and unloaded');
         setIsUnloaded(true);
         const uri = recording.getURI();
+        console.log('Retrieved URI:', uri);
         setAudioURI(uri);
+
         setIsAudioReadyToSend(true);
         setIsRecording(false);
         console.log('Recording stopped. Audio path: ', uri);
@@ -323,35 +206,6 @@ function SingleChatScreen({navigation, route}) {
     }
   }, [isRecording, recording, isUnloaded]);
 
-  //Function to send audio message to chat
-  const sendAudioMessage = useCallback(
-    async message => {
-      if (!message) return;
-      if (audioURI) {
-        const audioMessage = {
-          _id: Math.random().toString(),
-          // text: '',
-          createdAt: new Date(),
-          user: {
-            _id: 1,
-            avatar: require('../../../assets/Profile.png'),
-          },
-          audio: audioURI,
-        };
-        setMessages(previousMessages =>
-          GiftedChat.append(previousMessages, [audioMessage]),
-        );
-        setIsAudioReadyToSend(false);
-        //Reset the audioURI after sending
-        setAudioURI(null);
-        setIsRecording(false);
-        setRecording(null);
-        setIsUnloaded(false);
-      }
-    },
-    [audioURI],
-  );
-
   //Function to change the colour of text bubbles
   const renderBubble = props => {
     return (
@@ -359,7 +213,7 @@ function SingleChatScreen({navigation, route}) {
         {...props}
         textStyle={{
           left: {color: colors.darkGray},
-          right: {color: colors.grannySmithApple},
+          right: {color: colors.darkGray},
         }}
         wrapperStyle={{
           right: {backgroundColor: colors.grannySmithApple},
@@ -374,10 +228,9 @@ function SingleChatScreen({navigation, route}) {
       </View>
       <GiftedChat
         messages={messages}
-        // renderActions={renderActions}
         onSend={messages => onSend(messages)}
         user={{
-          _id: 1,
+          _id: userId,
         }}
         renderInputToolbar={props => (
           <CustomChatInput
@@ -386,6 +239,7 @@ function SingleChatScreen({navigation, route}) {
             startRecording={startRecording}
             stopRecording={stopRecording}
             isAudioReadyToSend={isAudioReadyToSend}
+            audioPath={audioURI}
             userId={userId}
           />
         )}
